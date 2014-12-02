7bbd15127cd6d92d28d178c53a61cf6317e92780
young <young.mwy@gmail.com>
2014-12-02 18:40:12 +0800

    print log before installing

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-cp ./bash_aliases ~/.bash_aliases
 echo "1. install ~/.bash_aliases"
+cp ./bash_aliases ~/.bash_aliases
 
-cp ./bash_aliases.android ~/.bash_aliases.android
 echo "2. install ~/.bash_aliases.android"
+cp ./bash_aliases.android ~/.bash_aliases.android
 
 if [[ -f ~/.bash_aliases.local ]]; then
     read -p "~/.bash_aliases.local exists! Do you want to overwrite it? [Y|n] : " answer
     if [[ ${answer} == 'Y' ]]; then
-        cp ./bash_aliases.local ~/.bash_aliases.local
         echo "3. update ~/.bash_aliases.local"
+        cp ./bash_aliases.local ~/.bash_aliases.local
         echo "install OK!"
     elif [[ ${answer} == 'n' ]]; then
         echo "install OK!"
