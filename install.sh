1437e4566b6f5be72aefe295769613249b31da3a
young <young.mwy@gmail.com>
2015-05-31 21:07:35 +0800

    add git status

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-echo "1. install ~/.bashrc"
+echo "1. install ~/.bashrc and ~/.git-prompt.sh"
 cp ./bashrc ~/.bashrc
+cp ./git-prompt.sh ~/.git-prompt.sh
 
 echo "2. install ~/.bash_aliases"
 cp ./bash_aliases ~/.bash_aliases
