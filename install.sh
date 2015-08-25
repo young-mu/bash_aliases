576d0670c17bda4cf67a787812fd08c085d2c1c5
young <young.mwy@gmail.com>
2015-08-25 23:42:06 +0800

    support android

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-echo "1. install ~/.bashrc"
+echo "1. install ~/.bashrc, ~/.adb.bash"
 cp ./bashrc ~/.bashrc
+cp ./adb.bash ~/.adb.bash
 
 echo "2. install ~/.bash_aliases"
 cp ./bash_aliases ~/.bash_aliases
+
+echo "3. install ~/.bash_aliases.android and ~/.proj.tar.gz"
+cp ./bash_aliases.android ~/.bash_aliases.android
+cp ./proj.tar.gz ~/.proj.tar.gz
 
 echo "install OK!"
