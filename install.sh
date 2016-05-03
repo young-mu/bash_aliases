143f8bd8bd676c2a20ae4892657cdbbe7ea06b38
MuYoung <Young@MuYoungs-MacBook-Pro.local>
2016-05-03 17:23:23 +0800

    remove android related conf. files by default

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 
 echo "1. install ~/.bashrc and ~/.adb.bash"
 cp ./bashrc ~/.bashrc
-cp ./adb.bash ~/.adb.bash
+#cp ./adb.bash ~/.adb.bash
 
 echo "2. install ~/.bash_aliases"
 cp ./bash_aliases ~/.bash_aliases
 
-echo "3. install ~/.bash_aliases.android and ~/.proj.tar.gz"
-cp ./bash_aliases.android ~/.bash_aliases.android
-cp ./proj.tar.gz ~/.proj.tar.gz
+#echo "3. install ~/.bash_aliases.android and ~/.proj.tar.gz"
+#cp ./bash_aliases.android ~/.bash_aliases.android
+#cp ./proj.tar.gz ~/.proj.tar.gz
 
 echo "install OK!"
